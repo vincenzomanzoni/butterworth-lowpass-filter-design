57ee2eb88fe5e23ce065e6eacef190a0cc96ae75
Vincenzo Manzoni <vincenzo.manzoni@gmail.com>
2023-02-07 16:40:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,14 @@ def butter_lowpass(cutoff, fs, order=5):
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
     return b, a
 
-def plot_filter_response(b, a, fs):
+def plot_filter_response(b, a, fs, width, height):
     w, h = freqz(b, a)
-    plt.figure(figsize=(6, 4))
+    plt.figure(figsize=(width, height))
     plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
     plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
     plt.axvline(cutoff, color='k')
     plt.xlim(0, 0.5*fs)
+    plt.grid(True)
     plt.title("Lowpass Filter Frequency Response")
     plt.xlabel("Frequency (Hz)")
     plt.ylabel("Gain")
@@ -30,7 +31,10 @@ cutoff = st.sidebar.number_input("Cutoff Frequency (Hz)", value=100, min_value=1
 
 b, a = butter_lowpass(cutoff, fs, order)
 
-plot_filter_response(b, a, fs)
+width = st.sidebar.number_input("Chart Width", value=6, min_value=1, max_value=20, step=1)
+height = st.sidebar.number_input("Chart Height", value=4, min_value=1, max_value=20, step=1)
+
+plot_filter_response(b, a, fs, width, height)
 st.pyplot()
 
 st.write("Numerator (b) Coefficients: ", b)
