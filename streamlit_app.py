42bd2019ec9232c91dfa90f90e4cf73820b6b353
Vincenzo Manzoni <vincenzo.manzoni@gmail.com>
2023-02-07 16:00:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,37 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import butter, freqz
 
-"""
-# Welcome to Streamlit!
+def butter_lowpass(cutoff, fs, order=5):
+    nyquist = 0.5 * fs
+    normal_cutoff = cutoff / nyquist
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def plot_filter_response(b, a, fs):
+    w, h = freqz(b, a)
+    plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
+    plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
+    plt.axvline(cutoff, color='k')
+    plt.xlim(0, 0.5*fs)
+    plt.title("Lowpass Filter Frequency Response")
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Gain")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title("Butterworth Lowpass Filter Coefficients Calculator")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+order = st.sidebar.slider("Filter Order", 1, 10, 5)
+fs = st.sidebar.slider("Sampling Frequency (Hz)", 1, 10000, 1000)
+cutoff = st.sidebar.slider("Cutoff Frequency (Hz)", 1, 10000, 100)
 
+b, a = butter_lowpass(cutoff, fs, order)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.write("Filter Order: ", order)
+st.write("Sampling Frequency: ", fs, "Hz")
+st.write("Cutoff Frequency: ", cutoff, "Hz")
+st.write("Numerator (b) Coefficients: ", b)
+st.write("Denominator (a) Coefficients: ", a)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+plot_filter_response(b, a, fs)
+st.pyplot()
