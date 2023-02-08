cff77a321b5b6f366a5e8b788ffedfd29ec19bfc
Vincenzo Manzoni <vincenzo.manzoni@gmail.com>
2023-02-08 17:58:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def plot_filter_response(b, a, fs, width, height):
     ax2.grid(False)
     plt.title("Butterworth Lowpass Filter Response")
 
-st.set_page_config(page_title="Butterworth Lowpass Filter Coefficients Calculator", page_icon=":guardsman:")
+st.set_page_config(page_title="Butterworth Lowpass Filter Coefficients Calculator")
 
 st.title("Butterworth Lowpass Filter Coefficients Calculator")
 
